2ff885e9f028f2e4a7d0221ad8d7236a5199d809
gurujustin <meanjustin2020@hotmail.com>
2020-12-23 13:15:37 -0500

    Initial broadway

---
+++ Broadway/app/controllers/plays_controller.rb
@@ -0,0 +1,51 @@
+class PlaysController < ApplicationController
+  before_action :find_play, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @plays = Play.all.order('created_at DESC')
+  end
+
+  def new
+    @play = Play.new
+  end
+
+  def create
+    @play = Play.new(play_params)
+    
+    if @play.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+
+  def show  
+  end
+
+  def edit
+  end
+
+  def update
+    if @play.update(play_params)
+      redirect_to play_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @play.destroy
+    redirect_to root_path
+  end
+
+  private
+    def play_params
+      @play = params.require(:play).permit(:title, :description, :director)
+    end
+
+    def find_play
+      @play = Play.find(params[:id])
+    end
+
+end
